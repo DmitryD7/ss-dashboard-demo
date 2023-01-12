5a9218113af61afea3a47ce047551afcf9aeabe2
DmitryDemid <dimon.demid15@gmail.com>
2023-01-12 12:40:36 -0500

    creating endpoints

---
+++ src/api/api.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+
+const instance = axios.create({
+    baseURL: 'https://account.stylescan.com/',
+    withCredentials: true,
+});
+
+export const accAPI = {
+    logout() {
+        return instance.get('logout');
+    },
+    billing() {
+        return instance.get('billing');
+    },
+    dashboard() {
+        return instance.get('account/dashboard.json');
+    },
+    setPassword(data: SetPasswordDataType) {
+        return instance.post('account/dashboard.json', data);
+    },
+};
+
+export const studioAPI = {
+    subscribe() {
+        return instance.post('/studio/subscribe');
+    },
+    unsubscribe() {
+        return instance.post('/studio/unsubscribe');
+    },
+};
+
+type SetPasswordDataType = {
+    pass0: string, // old password
+    pass1: string, // new password
+};
