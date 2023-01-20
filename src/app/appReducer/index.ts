e2a8a11b610cbcba3788e2f30a33e9007f2a1df1
DmitryDemid <dimon.demid15@gmail.com>
2023-01-20 16:50:48 -0500

    test developing

--- src/app/appReducer/index.ts
+++ src/app/appReducer/index.ts
@@ -2,14 +2,12 @@ import {appSlice} from "./appReducer";
 import {AppRootStateType} from "../types";
 
 const selectStatus = (state: AppRootStateType) => state.app.status;
-const selectIsInitialized = (state: AppRootStateType) => state.app.isInitialized;
 const selectError = (state: AppRootStateType) => state.app.error;
 
 const appReducer = appSlice.reducer;
 
 const appSelectors = {
     selectStatus,
-    selectIsInitialized,
     selectError,
 }
 
